f560e8f773fcbf1348ee3b4ddc400a60f41b5ede
imbelal <imbelalhosen@gmail.com>
2022-02-14 19:09:37 +0100

    Day 1: Added Syntax.py

---
+++ Day 1/Syntax.py
@@ -0,0 +1,17 @@
+# If you’ve been working in other programming languages such as Java, C#, or C/C++,
+# you know that these languages use semicolons (;) to separate the statements.
+# Python, however, uses whitespace and indentation to construct the code structure.
+# In Python, a single line comment begins with a hash (#) symbol followed by the comment.
+# define main funnction to print out something
+
+def main():
+    i = 0
+    max = 10
+
+    while (i < max):
+        print(i)
+        i = i + 1
+
+
+# call main function
+main()
