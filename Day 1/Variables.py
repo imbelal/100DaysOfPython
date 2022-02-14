fe62adf20fd22b6ddd818557bef965f90e6f998f
imbelal <imbelalhosen@gmail.com>
2022-02-14 19:34:32 +0100

    Day 1: Added - Variables.py added

---
+++ Day 1/Variables.py
@@ -0,0 +1,10 @@
+# In Python, a variable is a label that you can assign a value to it.
+# And a variable is always associated with a value.
+# The variable names should be as concise and descriptive as possible.
+# Also, they should adhere to Python variable naming rules.
+
+name = 'John'
+country = 'Bangladesh'
+name_and_country = name + ' from ' + country
+
+print(name_and_country)
