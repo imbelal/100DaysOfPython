087e772527475ae4713f495690d505812329dca9
imbelal <imbelalhosen@gmail.com>
2022-02-15 01:23:24 +0100

    Day 2: Added - Numbers.py added

---
+++ Day 2/Numbers.py
@@ -0,0 +1,11 @@
+# Python supports common numeric types including integers, floats, and complex numbers.
+# Use the underscores to group numbers for the large numbers.
+
+x = 5
+y = 10.23
+z = x * y
+print(z)
+
+# To make the long numbers more readable, you can group digits using underscores, like this:
+count = 10_000_000_000
+print(count)
