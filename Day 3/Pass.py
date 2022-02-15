fa97fa1eb41e31d01bebf3ba8bfa8c4c9fc5489a
imbelal <imbelalhosen@gmail.com>
2022-02-16 00:44:39 +0100

    Day 3: Added - Python pass statement added

---
+++ Day 3/Pass.py
@@ -0,0 +1,14 @@
+# Python pass statement
+# We can use pass statement in a situation where we want to pass a condition without writting any code
+# Have a look on the below examples
+
+counter = 1
+max = 10
+if counter <= max:
+    counter += 1
+else:
+    pass
+
+# The pass statement is a statement that does nothing.
+# It’s just a placeholder for the code that you’ll write in the future.
+# We can use pass statement with if else, for loop, while loop. function and class
