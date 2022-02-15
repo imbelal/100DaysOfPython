adbc8aa9319f6184a770a5179302a3580b5a922c
imbelal <imbelalhosen@gmail.com>
2022-02-15 01:37:31 +0100

    Day 2: Added - Operators.py added

---
+++ Day 2/Operators.py
@@ -0,0 +1,14 @@
+# Python has six comparison operators: less than (<), less than or equal to (<=), greater than (>),
+#  greater than or equal to (>=), equal to (==), and not equal to (!=).
+
+x = 5
+y = 8
+
+if(x <= 8):
+    print(x)
+
+# Logical Operators
+# Python has three logical operators: and, or, not
+
+if(x >= 5 and y <= 10):
+    print("It's logical!!")
