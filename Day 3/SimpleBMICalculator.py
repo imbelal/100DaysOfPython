e99d10e92289e2bbb49bdcf9218c7a13cb1c8369
imbelal <imbelalhosen@gmail.com>
2022-02-16 01:27:43 +0100

    Day 3: Update - BMI Calculator updated

--- Day 3/SimpleBMICalculator.py
+++ Day 3/SimpleBMICalculator.py
@@ -6,11 +6,14 @@
 
 
 from math import exp
+from typing import Tuple
 
-
-height = input("Enter your height in meter: ")
-weight = input("Enter your wieght in kg: ")
-# The Python ** operator is used to raise a number in Python to the power of an exponent.
-bmi = float(weight) / float(height) ** 2
-bmi = round(bmi, 2)
-print(f"BMI: {bmi}")
+command = 'y'
+while command.lower() == 'y':
+    height = input("Enter your height in meter: ")
+    weight = input("Enter your wieght in kg: ")
+    # The Python ** operator is used to raise a number in Python to the power of an exponent.
+    bmi = float(weight) / float(height) ** 2
+    bmi = round(bmi, 2)
+    print(f"BMI: {bmi}")
+    command = input("Do you want to calculate again? (y/n): ")
