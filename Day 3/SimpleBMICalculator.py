732e39ff0a818dd746999db0e15074d27da44e9c
imbelal <imbelalhosen@gmail.com>
2022-02-16 01:18:24 +0100

    Day 3: Update - BMI Calculator updated

--- Day 3/SimpleBMICalculator.py
+++ Day 3/SimpleBMICalculator.py
@@ -1,3 +1,10 @@
+'''
+    The BMI is a measure of some's weight taking into account their height. e.g. 
+    If a tall person and a short person both weigh the same amount, the short person is usually more overweight.
+    The BMI is calculated by dividing a person's weight (in kg) by the square of their height (in m):
+'''
+
+
 from math import exp
 
 
